01004bed84174ee583eff6d985d1665dbbd3ba63
Enoch1357 <enochmensah1357@gmail.com>
2023-07-21 15:12:01 +0000

    python hello world done

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,5 @@
 for i in range(10):
     for k in range(10):
         number = str("{}{}".format(i, k)) 
-        if i != k and i < k:
-            print("{:02}".format(number), end=",")
+        if i != k and i < k and number!=89:
+            print("{:02},".format(number), end=" ")
