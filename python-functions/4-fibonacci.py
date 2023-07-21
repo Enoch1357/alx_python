ec840b1752cf2af847fabffaf051eaf7be7fcc2c
Enoch1357 <enochmensah1357@gmail.com>
2023-07-21 14:45:31 +0000

    4-fibonacci.py last update!

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,24 +1,23 @@
-n = int(input())
+n = int()
+def fibonacci_sequence(n):
+   n1 = 0
+   n2 = 1
+   count = 0
+   if n <= 0:
+      print("[", end="")
+   elif n == 1:
+      print("[{}".format(0), end="")
+   elif n ==2:
+      print("[{},".format(n1), end=" ")
+   else:
+      print("[", end="")
+      while count < n - 1:
+         print("{},".format(n1), end=" ")
+         nth = n1 + n2
 
-# first two terms
-n1 = 0
-n2 = 1
-count = 0
-
-# check if the number of terms is valid
-if n <= 0:
-   print(0)
-# if there is only one term, return n1
-elif n == 1:
-   print([n1])
-# generate fibonacci sequence
-elif n ==2:
-    print("[{}, {}]".format(n1, n2))
-else:
-   while count < n:
-       print("{},".format(n1), end=" ")
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+         n1 = n2
+         n2 = nth
+         count += 1
+         if count == n -1:
+            print("{}".format(n1), end="")
+   return ("]")
