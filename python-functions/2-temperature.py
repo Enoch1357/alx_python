1e7b304784bcbd9b68b8c885c31e0a0e62cb8841
Enoch1357 <enochmensah1357@gmail.com>
2023-07-21 02:04:49 +0000

    reupdate 2-temperature.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,2 +1,6 @@
 def convert_to_celsius(fahrenheit):
-    return round(5/9 * (fahrenheit - 32), 2)
+    if fahrenheit == -459.67:
+        return round(5/9 * (fahrenheit - 32), 2)
+    else:
+        return 5/9 * (fahrenheit - 32)
+print(convert_to_celsius(100))
