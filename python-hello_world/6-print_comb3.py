ea284ce0ab1c3241f4d7590d251d6273b6af83c1
Enoch1357 <enochmensah1357@gmail.com>
2023-07-23 20:02:30 +0000

    Finally I see

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@ for i in range(10):
     for k in range(10):
         number = str("{}{}".format(i, k)) 
         if i != k and i < k and int(number) != 89:
-            print("{:02}".format(number), end=", ")
+            print("{:02}".format(int(number)), end=", ")
         elif i != k and i < k and int(number) == 89:
-            print("{:02}".format(number))
+            print("{:02}".format(int(number)))
