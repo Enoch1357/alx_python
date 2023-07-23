dc5dbc1da919eb3929d66038823547b8786f289f
Enoch1357 <enochmensah1357@gmail.com>
2023-07-23 19:56:46 +0000

    6-print_comb3.py finally rectified fully

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,7 @@
 for i in range(10):
     for k in range(10):
         number = str("{}{}".format(i, k)) 
-        if i != k and i < k and number!=89:
+        if i != k and i < k and int(number) != 89:
             print("{:02},".format(number), end=" ")
+        elif i != k and i < k and int(number) == 89:
+            print("{:02}".format(number))
