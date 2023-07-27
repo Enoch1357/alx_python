de90b34f38bb0c1c73ad95325e5d9630be8efacc
Enoch1357 <enochmensah1357@gmail.com>
2023-07-27 16:41:36 +0000

    3-safe_print_division.py pushed

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        quotient = int(a) / int(b)
+        result = quotient
+        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
+    except ZeroDivisionError:
+        result = "None"
+        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
+    return "\n"
