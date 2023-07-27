249f81eed72dacf740fc7555e84113570649039b
Enoch1357 <enochmensah1357@gmail.com>
2023-07-27 01:12:28 +0000

    1-args.py updated

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,21 @@
 #! /usr/bin/python3
 __name__ == "__main__"
-def lister(*arguments):
-    no_of_arguments = len(arguments)
-    counter = 1
-    if no_of_arguments == 0:
-        print("{} arguments.".format(no_of_arguments), end="\n")
-    elif no_of_arguments == 1:
-        print("{} argument:".format(no_of_arguments), end="\n")
+string_input = str(input())
+arguments = string_input.split(" ")
+no_of_arguments = len(arguments)
+counter = 1
+if arguments == ['']:
+    print("{} arguments.".format(0), end="\n")
+elif no_of_arguments == 1:
+    print("{} argument:".format(no_of_arguments), end="\n")
+    for argument in arguments:
         print("{}: {}".format(counter, argument), end="\n")
-    else:
-        print("{} arguments:".format(no_of_arguments), end="\n")
-        for argument in arguments:
-            while counter <= no_of_arguments:
-                argument = arguments[counter -1]
-                print("{}: {}".format(counter, argument), end="\n")
-                nth = arguments[counter - 1]
-                argument = nth
-                counter += 1
-    return ("\n")
+else:
+    print("{} arguments:".format(no_of_arguments), end="\n")
+    for argument in arguments:
+        while counter <= no_of_arguments:
+            argument = arguments[counter -1]
+            print("{}: {}".format(counter, argument), end="\n")
+            nth = arguments[counter - 1]
+            argument = nth
+            counter += 1
