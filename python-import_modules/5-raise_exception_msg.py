e6bdf0c749fa4065068d7fb3faab0448b994cef7
Enoch1357 <enochmensah1357@gmail.com>
2023-07-27 22:37:00 +0000

    5-raise_exception_msg.py pushed

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+l1 = [1,2,5,8,9]
+l2 = [1,5.6,"hello"]
+l1[1] = 100
+def raise_exception_msg(message=""):
+    
+    try:
+        del l[2]
+        print(l1)
+    except NameError:
+        print("C is fun")
+    finally:
+        return(l1)
