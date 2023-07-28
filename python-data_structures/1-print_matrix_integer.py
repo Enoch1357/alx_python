6ca8330a3c5a28dbdff9cf806daf8f6a314d127a
Enoch1357 <enochmensah1357@gmail.com>
2023-07-28 02:26:57 +0000

    1-print_matrix_integer.py reupdated

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
 def print_matrix_integer(matrix=""):
     for i in matrix:
+        if matrix == [[]]:
+            print(" ")
         without_last_member = i[:-1]
         for k in without_last_member:
             print("{:d}".format(k), end=" ")
