a4fcc64180876894030248821b31e2be7957203e
Enoch1357 <enochmensah1357@gmail.com>
2023-07-28 01:20:32 +0000

    1-print_matrix_integer.py update

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=""):
     for i in matrix:
         for k in i:
-            print("{}".format(k), end=" ")
+            print("{:d}".format(k), end=" ")
         print(end="\n")
     return matrix
