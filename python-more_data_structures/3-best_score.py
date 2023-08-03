6ce4da103354ef8c7a03244e181c479d1b72eca8
Enoch1357 <enochmensah1357@gmail.com>
2023-08-03 17:48:12 +0000

    3-best_score.py updated

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     try:
         max_value_key = max(a_dictionary, key=a_dictionary.get)
-    except AttributeError:
+    except (AttributeError, ValueError):
         max_value_key = None
     return max_value_key
