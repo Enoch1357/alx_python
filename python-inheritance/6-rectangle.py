79d58edb26a115ef4cd35c1128fd853443e02f6c
Enoch1357 <enochmensah1357@gmail.com>
2023-09-05 20:01:54 +0000

    6-rectangle.py final update

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -7,7 +7,23 @@ class Rectangle(BaseGeometry):
     """
     This class inherits from another class 'BaseGeometry'.
     """
+    def __dir__(cls) -> None:
+        """Removes __init_subclass__"""
+        attributes = super().__dir__()
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
     def __init__(self, width, height):
         """This method instantiates the class with the private attributes width and height."""
         self.__width = width
+        if type(self.__width) != int:
+            raise TypeError("width must be an integer")
+        
+        elif self.__width <= 0:
+            raise ValueError("width must be greater than 0")
+        
         self.__height = height
+        if type(self.__height) != int:
+            raise TypeError("height must be an integer")
+        
+        elif self.__height <= 0:
+            raise ValueError("height must be greater than 0")
