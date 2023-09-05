5bc7c0c69ab272d0a1216945538ad979765d85fb
Enoch1357 <enochmensah1357@gmail.com>
2023-09-05 21:29:15 +0000

    8-square.py pushed

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+"""
+This module contains a class 'Square'.
+"""
+Rectangle = __import__('7-rectangle').Rectangle
+class Square(Rectangle):
+    """
+    This is a class that inherits from another class 'Rectangle'
+    """
+
+    def __init__(self, size):
+        """This method instantiates the class with the private attribut 'size'."""
+        
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("width must be an integer")
+        
+        elif self.__size <= 0:
+            raise ValueError("width must be greater than 0")
+
+    def area(self):
+        """This method computes the area of a square given it's size attribute"""
+
+        self.area = self.__size ** 2
+        return self.area
+
+    def __str__(self):
+        """This methods overrides and assigns a new string representation for the instance object of the class"""
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
