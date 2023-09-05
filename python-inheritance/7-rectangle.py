59fa7cf219ecbcd555fe3262ab0968af418ab47a
Enoch1357 <enochmensah1357@gmail.com>
2023-09-05 20:54:32 +0000

    7-rectangle.py pushed

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+"""
+This module contains a class that inherits from another class 'BaseGeometry'
+"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    """
+    This class inherits from another class 'BaseGeometry'.
+    """
+    def __dir__(cls) -> None:
+        """Removes __init_subclass__"""
+        attributes = super().__dir__()
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    def __init__(self, width, height):
+        """This method instantiates the class with the private attributes width and height."""
+        
+        self.__width = width
+        if type(self.__width) != int:
+            raise TypeError("width must be an integer")
+        elif self.__width <= 0:
+            raise ValueError("width must be greater than 0")
+        
+        self.__height = height
+        if type(self.__height) != int:
+            raise TypeError("height must be an integer")
+        elif self.__height <= 0:
+            raise ValueError("height must be greater than 0")
+
+    def area(self):
+        """This method computes the area of a rectangle given it's width and height attributes"""
+
+        self.area = self.__width * self.__height
+        return self.area
+
+    def __str__(self):
+        """This methods overrides and assigns a new string representation for the instance object of the class"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
