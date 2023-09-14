e441558dc5b63e2f2ca4ed78d466af46cbaa20a5
Enoch1357 <enochmensah1357@gmail.com>
2023-09-14 01:40:33 +0000

    square.py finally pushed

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,4 +13,16 @@ class Square(Rectangle):
     def __str__(self):
         """This method overrides the string representation for the class instance."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+
+    @property
+    def size(self):
+        return self.width
+
+    @size. setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("{} must be an integer".format('width'))
+        elif size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
