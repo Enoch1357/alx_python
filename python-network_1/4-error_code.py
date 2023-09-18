f0cb3f8ec6174a455e6e08d69c0df2f580d5e232
Enoch1357 <enochmensah1357@gmail.com>
2023-09-18 03:16:07 +0000

    4-error_code.py pushed

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+"""
+This module takes in a URL, sends a request to the URL and displays the body of the response.
+"""
+import sys
+import requests
+url = sys.argv[1]
+request = requests.get(url)
+if request.status_code >= 400:
+    print("Error code: {}".format(request.status_code))
+else:
+    print("Index")
