0c39296a0b3b05d0b5d24529d97af3313d88bbdb
Enoch1357 <enochmensah1357@gmail.com>
2023-09-18 01:07:34 +0000

    0-hbtn_status.py updated again

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,5 +5,5 @@ This module fetches the url 'https://alu-intranet.hbtn.io/status'.
 import requests
 request = requests.get('https://alu-intranet.hbtn.io/status')
 print("Body response:")
-print("    - type: {}".format(type(request.text)))
-print("    - content: {}".format(request.text))
+print("\t- type: {}".format(type(request.text)))
+print("\t- content: {}".format(request.text))
