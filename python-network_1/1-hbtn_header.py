5d2b481e6141cbd197f9f1f84631b1986c313bd7
Enoch1357 <enochmensah1357@gmail.com>
2023-09-18 01:24:59 +0000

    1-hbtn_header.py pushed

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+"""
+This module takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header.
+"""
+import requests, sys
+def req_sender(url):
+    request = requests. get(url)
+    print(request.headers[X-Request-Id])
