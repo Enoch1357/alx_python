24d79f62a8e3fbd6269fa5aad4765ae4c1dc0b9a
Enoch1357 <enochmensah1357@gmail.com>
2023-09-18 04:39:44 +0000

    6-my_github.py pushed

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+"""
+This module takes your GitHub credentials (username and password) and uses the GitHub API to display your id.
+"""
+import sys
+import requests
+username = sys.argv[1]
+password = sys.argv[2]
+
+url = 'https://api.github.com/user'
+auth = (username, password)
+
+try:
+    request = requests.get(url, auth=auth)
+    response = request.json()
+
+    # Display the user's ID
+    print(response['id'])
+
+except ValueError:
+    print("Not a valid JSON response")
+
