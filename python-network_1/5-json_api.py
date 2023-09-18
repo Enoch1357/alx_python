d3e63f26e537beebd0bf0ef09327e781befd226e
Enoch1357 <enochmensah1357@gmail.com>
2023-09-18 03:47:12 +0000

    5-json_api.py pushed

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+"""
+This module takes in a letter and sends a POST request to 'http://0.0.0.0:5000/search_user' with the letter as a parameter.
+"""
+import sys
+import requests
+if len(sys.argv) == 1:
+    q = ""
+elif len(sys.argv) == 2:
+    q = sys.argv[1]
+request = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+user = request.json()
+if user:
+    print("[{}] {}".format(user['id'], user['name']))
+elif user == {}:
+    print('No result')
+else:
+    print("Not a valid JSON")
