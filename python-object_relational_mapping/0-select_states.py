9964a48585551e66ff32bcc59c01d26fdb1e63c6
Enoch1357 <enochmensah1357@gmail.com>
2023-09-26 20:20:30 +0000

    0-select_states.py updated

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ password = sys.argv[2]
 database_name = sys.argv[3]
 database = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database_name)
 cursor = database.cursor()
-cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id  ORDER BY cities.id ASC")
+cursor.execute("SELECT * FROM states  ORDER BY states.id ASC")
 
 cursor.close()
 database.close()
