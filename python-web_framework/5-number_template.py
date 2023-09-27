ffdafb86cff3a743e1dffb15ee2ea135c0136952
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 17:33:36 +0000

    updates

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -30,7 +30,6 @@ def is_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    n = 1
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
