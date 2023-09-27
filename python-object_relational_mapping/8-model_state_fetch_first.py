4af49f68241d201e8cb14b56000213f89197b09f
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 15:19:12 +0000

    8-model_state_fetch_first.py update

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,7 +16,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 first_state = session.query(State).order_by(State.id).first()
 if first_state is None:
-    print("Nothing\n")
+    print("Nothing", end="\n")
 else:
     print(f"{first_state.id}: {first_state.name}")
 session.close()
