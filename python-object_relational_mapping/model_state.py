a5f3cd4d049949ac7a01fbbe88762debe7879750
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 14:16:12 +0000

    model_state.py update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,7 +6,9 @@ This module contains a class definition of a state;
 import sqlalchemy
 Base = declarative_base()
 class State(Base):
-    """This is a class State that inherits from the class Base"""
+    """
+    This is a class 'State' that inherits from the class 'Base'
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
