29bd7037eb34e9bd7ab25a48db9d6b5634f69d7a
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 15:39:30 +0000

    9-model_state_filter_a.py update

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,8 +15,8 @@ engine = create_engine(f"mysql+mysqldb://{username}:{password}\
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-states_with_a = session.query(State).filter(
-                                            State.name.like('%a%')).order_by(State.id).all()
+states_with_a = session.query(State)\
+                .filter(State.name.like('%a%')).order_by(State.id).all()
 for state in states_with_a:
     print(f"{state.id}: {state.name}")
 session.close()
