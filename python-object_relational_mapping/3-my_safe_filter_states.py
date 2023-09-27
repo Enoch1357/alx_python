1e88e6f11d849c2ce505c061d876e4193c8ed9ea
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 00:46:42 +0000

    3-my_safe_filter_states.py pushed

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+"""
+This module takes in an argument and displays all values
+in the 'states' table of 'hbtn_0e_0_usa';
+where name matches the argument
+"""
+import MySQLdb
+import sys
+__name__ == "__main__"
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+state_name = sys.argv[4]
+host = "localhost"
+db = MySQLdb.connect(host=host, user=username,
+                     passwd=password, db=database_name)
+cursor = db.cursor()
+query = "SELECT * FROM states WHERE BINARY name LIKE %s ORDER BY states.id ASC"
+cursor.execute(query, ["{}".format(state_name)])
+states = cursor.fetchall()
+for state in states:
+    print(state)
+cursor.close()
+db.close()
