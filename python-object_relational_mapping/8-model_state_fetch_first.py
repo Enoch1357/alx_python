ded51c80c1e4f671fdaadfdec60b1f67bd6db16a
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 15:15:27 +0000

    8-model_state_fetch_first.py update

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,7 +2,8 @@
 """
 This module  prints the first State object from the database hbtn_0e_6_usa
 """
-import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 username = sys.argv[1]
