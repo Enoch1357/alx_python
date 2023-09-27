2b308bd58df7640fac8497e964078f6e5542a91e
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 14:25:18 +0000

    model_state.py update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,9 +10,10 @@ Base = declarative_base()
 
 class State(Base):
     """This is a class State that inherits from the class Base"""
-    
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+
