2ae3b6d4b98be5a05f636b23b0961c036e5573f9
Enoch1357 <enochmensah1357@gmail.com>
2023-09-27 13:31:38 +0000

    5-filter_cities.py pushed

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#! /usr/bin/pyhton3
+"""
+This module takes in the name of a state as an argument
+and lists all 'cities' of that state,
+using the database 'hbtn_0e_4_usa'
+"""
+import MySQLdb
+import sys
+__name__ == "__main__"
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+state_name = sys.argv[4]
+db = MySQLdb.connect(host="localhost", user=username,
+                     passwd=password, db=database_name)
+cursor = db.cursor()
+query = "SELECT cities.name FROM cities \
+        JOIN states ON cities.state_id = states.id \
+        ORDER BY cities.id ASC" 
+cursor.execute(query, ["{}".format(state_name)])
+cities = cursor.fetchall()
+for city in cities:
+    print(city)
+cursor.close()
+db.close()
