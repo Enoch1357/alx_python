92e4f31ddfb792b471900056e3985e9c281f6cf2
Enoch1357 <enochmensah1357@gmail.com>
2023-10-04 18:42:47 +0000

    5-json_api.py updated

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -12,12 +12,17 @@ else:
 try:
     request = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     request.raise_for_status()
-    json_response = request.json()
 
-    if json_response:
-        print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
-    else:
-        print("No result")
+    try:
+        json_response = request.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
 
 except requests.exceptions.RequestException as e:
     print("Not a valid JSON")
