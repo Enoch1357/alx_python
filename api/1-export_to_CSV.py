e581d9b86dad9299dd928fa5bec16a4995561597
Enoch1357 <enochmensah1357@gmail.com>
2023-10-08 18:58:44 +0000

    1-export_to_CSV.py update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,40 +1,32 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 """
 This module export specific data in the CSV format.
 """
-#! /usr/bin/python3
-"""
-This script accesses data from an API 'REST API',
-for a given employee ID, then returns information
-about his/her todo list progress.
-"""
-import sys
-import requests
 import csv
+import requests
+import sys
 def getTodoInfo(employee_id):
     """
     This function takes in an integer parameter and 
     returns info of the employee that corresponds to the
     parameter given 'employee_id'
     """
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-
-    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    request = requests.get(todos_url)
-    tasks = request.json()
-    total_tasks = len(tasks)
-    completed_tasks = sum(1 for task in tasks if task["completed"])
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    resp = requests.get(employee_url).json()
+    name = resp.get('name')
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    tasks = requests.get(todos_url).json()
 
     csvfileName = f"{employee_id}.csv"
     with open(csvfileName, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file)
-    for task in tasks:
-        writer.writerow([employee_id, employee_data['name'], task['completed'], task['title']])
+
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in tasks:
+            writer.writerow([employee_id, name, task.get('completed'), task.get('title')])
 
 if len(sys.argv) != 2:
     print("Usage error: python <script> <employee-id>")
 else:
-    employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
     getTodoInfo(employee_id)
