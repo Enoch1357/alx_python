99a7ce5f3414fef6b41d84fadf0257d4f96c2e90
Enoch1357 <enochmensah1357@gmail.com>
2023-10-08 17:42:32 +0000

    0-gather_data_from_an_API.py update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,21 +12,28 @@ def getTodoInfo(employee_id):
     returns info of the employee that corresponds to the
     parameter given 'employee_id'
     """
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    users_url = f'https://jsonplaceholder.typicode.com/users/'
     employee_response = requests.get(employee_url)
     employee_data = employee_response.json()
 
-    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    request = requests.get(todos_url)
-    tasks = request.json()
-    total_tasks = 0
-    completed_tasks = 0
-    for i in tasks:
+    todos_url = f'https://jsonplaceholder.typicode.com/users//todos'
+    todos_count = 0
+    todos_done = 0
+
+    request = requests.get(todos_url).json()
+    for i in request:
         if i['userId'] == employee_id:
-            total_tasks += 1
+            todos_count += 1
         if (i['completed'] and i['userId'] == employee_id):
-            completed_tasks += 1
-    print("Employee {} is done with tasks({}/{}):".format(employee_data.get('name'), completed_tasks, tasks))
+            todos_done += 1
+
+    request = requests.get(users_url).json()
+
+    name = None
+    for i in request:
+        if i['id'] == employee_id:
+            name = i['name']
+    print("Employee {} is done with tasks({}/{}):".format(name, todos_done, todos_count))
     for task in tasks:
         if task.get('completed') == True:
             print(f"\t {task.get('title')}")
