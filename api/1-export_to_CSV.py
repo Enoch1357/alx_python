5e301f22d73d879760dbcaef07fec6567eb847d9
Enoch1357 <enochmensah1357@gmail.com>
2023-10-08 19:11:19 +0000

    1-export_to_CSV.py update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+#! /usr/bin/python3.4.3
 """
-This module export specific data in the CSV format.
+This script accesses data from an API 'REST API',
+for a given employee ID, then returns information
+about his/her todo list progress.
 """
-import csv
 import requests
 import sys
+import csv
+
 def getTodoInfo(employee_id):
     """
     This function takes in an integer parameter and 
@@ -12,18 +15,37 @@ def getTodoInfo(employee_id):
     parameter given 'employee_id'
     """
     users_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    resp = requests.get(employee_url).json()
-    name = resp.get('name')
     todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
-    tasks = requests.get(todos_url).json()
+    todos_done = 0
 
-    csvfileName = f"{employee_id}.csv"
-    with open(csvfileName, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
+    resp = requests.get(todos_url).json()
+    todos_count = len(resp)
+    for i in resp:
+        if (i.get('completed')):
+            todos_done += 1
 
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in tasks:
-            writer.writerow([employee_id, name, task.get('completed'), task.get('title')])
+    resp = requests.get(users_url).json()
+    name = resp.get('name')
+
+    print("Employee {} is done with tasks({}/{}):".format(name, todos_done, todos_count))        
+    employee_todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    todos = requests.get(employee_todo_url).json()
+
+    # Create a CSV file
+    csv_filename = "{}.csv".format(employee_id)
+    with open(csv_filename, 'w', newline='') as csvfile:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for task in todos:
+            writer.writerow({
+                "USER_ID": employee_id,
+                "USERNAME": name,
+                "TASK_COMPLETED_STATUS": task['completed'],
+                "TASK_TITLE": task['title']
+            })
+
 
 if len(sys.argv) != 2:
     print("Usage error: python <script> <employee-id>")
